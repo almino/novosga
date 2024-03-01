cd960df0fc9efd124c7de24434602f11a6b6773b
Almino Melo <almino@sobral.ufc.br>
2024-03-01 11:54:08 -0300

    Ambiente de desenvolvimento usando o Nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+# https://github.com/nix-community/nix-direnv/blob/4132068df5153d44b7c17401ccefe54f99736dc1/README.md#usage-example
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    php82
+    php82Extensions.bcmath
+    php82Extensions.gettext
+    php82Extensions.intl
+    php82Extensions.mysqli
+    php82Extensions.opcache
+    php82Extensions.pdo
+    php82Extensions.pdo_mysql
+    php82Packages.composer
+    php82Packages.phan
+    php82Packages.php-cs-fixer
+    php82Packages.phpcs
+  ];
+}
